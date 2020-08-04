552bdbcaa7edcc34bf182d6b512724e4c2ae4495
WMartinez44 <>
2020-08-04 19:16:34 -0400

    adding restful services to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,8 +13,8 @@ const routes: Routes = [
     {path: '',   redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'character', component: CharactersComponent },
-    { path: 'character/:id', component: CharacterDetailsComponent},
+    { path: 'Characters', component: CharactersComponent },
+    { path: 'Character/:id', component: CharacterDetailsComponent},
     { path: '**', component: PageNotFoundComponent}
 ];
 
